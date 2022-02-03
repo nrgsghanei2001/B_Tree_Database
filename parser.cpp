1784018407259101dc7629ea018e39cbf584dc5d
nargesghanei2001 <nargesghanei031@gmail.com>
2022-02-04 00:04:52 +0330

    add query interpretor for CREATE

--- parser.cpp
+++ parser.cpp
@@ -5,11 +5,17 @@
 using namespace std;
 
 
-int main() {
-    vector<string> tokens;       // saving tokens
-    string input, word = "";
-    getline(cin, input);        // get query line
-    
+class Query {
+    private:
+        vector<string> tokens;
+    public:
+        void parse_query(string);
+        vector<string> get_tokens();
+};
+//////////////////////////////////////////////////////////
+void Query::parse_query(string input) {
+    string word = "";
+ 
     // move on query line
     for (int i = 0; i < input.length(); i++) {
         // when got to space just ignore it and add created word till now as a token
@@ -46,6 +52,122 @@ int main() {
     }
     for(int i = 0; i < tokens.size(); i++)
         cout << tokens[i] << '\n';
+}
+////////////////////////////////////////////////////////
+vector<string> Query::get_tokens() {
+    return tokens;
+}
+///////////////////////////////////////////////////////
+void ORM(vector<string> tokens) {
+    string operation = tokens[0];
+    // first token shows operation to call the right method
+    if (operation == "CREATE") {
+        string table = tokens[1];    // second token should be TABLE
+        if (table == "TABLE") {
+            string table_name = tokens[2];         // third token should be name of table we wanna create
+
+
+        }
+    }
+    else if (operation == "INSERT") {
+        string into = tokens[1];    // second token should be INTO
+        if (into == "INTO") {
+            string table_name = tokens[2];         // third token should be name of table we wanna insert to
+        }
+    }
+    else if (operation == "DELETE") {
+        string from = tokens[1];    // second token should be FROM
+        if (from == "FROM") {
+            string table_name = tokens[2];         // third token should be name of table we wanna delete from
+        }
+    }
+    else if (operation == "SELECT") {
+
+    }
+    else if (operation == "UPDATE") {
+
+    }
+}
+////////////////////////////////////////////////////////
+class Table {
+    private:
+        string table_name;
+        int table_id;
+        vector<vector<string>> fields;
+    public:
+        Table(string);
+        string get_name();
+        void create_filed(string, string);
+        void show_fields();
+};
+////////////////////////////////////////////////////////
+Table::Table(string name) {
+    table_name = name;
+    fields.push_back({});
+    fields.push_back({});
+}
+////////////////////////////////////////////////////////
+string Table::get_name() {
+    return table_name;
+}
+///////////////////////////////////////////////////////
+void Table::show_fields() {
+    for (int i = 0; i < fields[0].size(); i++) {
+        cout << i + 1 << "-" << fields[0][i] << " " << fields[1][i] << endl;
+    }
+}
+//////////////////////////////////////////////////////
+void Table::create_filed(string field_name, string field_type) {
+    // supported type: string, date, int
+    if (field_type == "string" || field_type == "timestamp" || field_type == "int") {
+        fields[0].push_back(field_name);
+        fields[1].push_back(field_type);
+    }
+}
+///////////////////////////////////////////////////////
+int main() {
+    string input;
+    getline(cin, input);
+    Query query;
+    query.parse_query(input);
+    vector<string> tokens = query.get_tokens();
+    vector <Table*> all_tables;
+    // turn tokens to sql code
+    string operation = tokens[0];
+    // first token shows operation to call the right method
+    if (operation == "CREATE") {
+        string table = tokens[1];    // second token should be TABLE
+        if (table == "TABLE") {
+            string table_name = tokens[2];         // third token should be name of table we wanna create
+            all_tables.push_back(new Table(table_name));   // add object of table to all table objects list
+            Table* table_obj = all_tables[all_tables.size() - 1];
+
+            // add fields of table to it
+            for (int i = 3; i < tokens.size(); i+=2) {
+                table_obj->create_filed(tokens[i], tokens[i + 1]);
+            }
+            table_obj->show_fields();
+        }
+    }
+    else if (operation == "INSERT") {
+        string into = tokens[1];    // second token should be INTO
+        if (into == "INTO") {
+            string table_name = tokens[2];         // third token should be name of table we wanna insert to
+        }
+    }
+    else if (operation == "DELETE") {
+        string from = tokens[1];    // second token should be FROM
+        if (from == "FROM") {
+            string table_name = tokens[2];         // third token should be name of table we wanna delete from
+        }
+    }
+    else if (operation == "SELECT") {
+
+    }
+    else if (operation == "UPDATE") {
+
+    }
+    
 
     return 0;
 }
